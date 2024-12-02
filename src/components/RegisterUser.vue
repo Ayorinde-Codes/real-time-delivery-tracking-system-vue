6b1ce97affef8a8eedbfb60627a9888e8383d9a1
Ayorinde Akindemowo <ayorindeakindemowo@Ayorindes-MacBook-Pro.local>
2024-12-02 23:24:30 +0100

    add-register-page

---
+++ src/components/RegisterUser.vue
@@ -0,0 +1,37 @@
+<template>
+  <form @submit.prevent="registerUser">
+    <input v-model="name" placeholder="Name" required />
+    <input v-model="email" placeholder="Email" type="email" required />
+    <input v-model="password" placeholder="Password" type="password" required />
+    <button type="submit">Register</button>
+  </form>
+</template>
+
+<script>
+import api from "../services/api";
+
+export default {
+  data() {
+    return {
+      name: "",
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    async registerUser() {
+      try {
+        const response = await api.post("/register", {
+          name: this.name,
+          email: this.email,
+          password: this.password,
+        });
+        alert(response.data.message);
+      } catch (error) {
+        console.error(error);
+        alert("Failed to register user");
+      }
+    },
+  },
+};
+</script>
